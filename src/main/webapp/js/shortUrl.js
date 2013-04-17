17ae83b5ae1178b002ed2508cba6af2c5d975456
Marcus Bell <marbell@csumb.edu>
2013-04-16 19:56:52 -0700

    added url shortening

---
+++ src/main/webapp/js/shortUrl.js
@@ -0,0 +1,20 @@
+var login = "marcb70";
+var api_key = "R_9d2db1de70dae269b6ee91cd96bd44b0";
+
+
+function get_short_url(long_url, login, api_key, func)
+{
+    $.getJSON(
+        "http://api.bitly.com/v3/shorten?callback=?", 
+        { 
+            "format": "json",
+            "apiKey": api_key,
+            "login": login,
+            "longUrl": long_url
+        },
+        function(response)
+        {
+            func(response.data.url);
+        }
+    );
+}
