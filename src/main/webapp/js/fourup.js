6a92110744ba2f9e1bc1a409f60bf18a0b1e0878
leighannewarner <leighannewarner@gmail.com>
2013-04-20 20:35:05 -0700

    Put back in static URL to fix broken url generation

--- src/main/webapp/js/fourup.js
+++ src/main/webapp/js/fourup.js
@@ -1,71 +1,71 @@
 var _gaq = _gaq || [];
-	_gaq.push([ '_setAccount', 'UA-39027752-1' ]);
-	_gaq.push([ '_trackPageview' ]);
-	(function() {
-		var ga = document.createElement('script');
-		ga.type = 'text/javascript';
-		ga.async = true;
-		ga.src = ('https:' == document.location.protocol ? 'https://ssl'
-				: 'http://www')
-				+ '.google-analytics.com/ga.js';
-		var s = document.getElementsByTagName('script')[0];
-		s.parentNode.insertBefore(ga, s);
-	})();
+_gaq.push([ '_setAccount', 'UA-39027752-1' ]);
+_gaq.push([ '_trackPageview' ]);
+(function() {
+	var ga = document.createElement('script');
+	ga.type = 'text/javascript';
+	ga.async = true;
+	ga.src = ('https:' == document.location.protocol ? 'https://ssl'
+			: 'http://www')
+			+ '.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(ga, s);
+})();
 
 function getParameterByName(name) {
-		name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
-		var regexS = "[\\?&]" + name + "=([^&#]*)";
-		var regex = new RegExp(regexS);
-		var results = regex.exec(window.location.search);
-		if (results == null)
-			return "";
-		else
-			return decodeURIComponent(results[1].replace(/\+/g, " "));
-	}
-	$(document)
-			.ready(
-					function() {
+	name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+	var regexS = "[\\?&]" + name + "=([^&#]*)";
+	var regex = new RegExp(regexS);
+	var results = regex.exec(window.location.search);
+	if (results == null)
+		return "";
+	else
+		return decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+$(document)
+		.ready(
+				function() {
 
-						var isURI = function(element) {
-							return /^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i
-									.test(element);
+					var isURI = function(element) {
+						return /^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i
+								.test(element);
+					}
+
+					var fillFrames = function() {
+						urls = [ getParameterByName("first"),
+								getParameterByName("second"),
+								getParameterByName("third"),
+								getParameterByName("fourth") ];
+						if (urls[0] == "" && urls[1] == "" && urls[2] == ""
+								&& urls[3] == "") {
+							$("#firstFrame").attr('src', "info.html");
+							$("#secondFrame").attr('src', "gen.html");
+							$("#thirdFrame").attr('src', "example.html");
+							$("#fourthFrame").attr('src', "howto.html");
+						} else {
+							if (isURI(urls[0])) {
+								$("#firstFrame").attr('src', urls[0]);
+							} else {
+								$("#firstFrame").attr('srcdoc', urls[0]);
+							}
+							if (isURI(urls[1])) {
+								$("#secondFrame").attr('src', urls[1]);
+							} else {
+								$("#secondFrame").attr('srcdoc', urls[1]);
+							}
+							if (isURI(urls[2])) {
+								$("#thirdFrame").attr('src', urls[2]);
+							} else {
+								$("#thirdFrame").attr('srcdoc', urls[2]);
+							}
+							if (isURI(urls[3])) {
+								$("#fourthFrame").attr('src', urls[3]);
+							} else {
+								$("#fourthFrame").attr('srcdoc', urls[3]);
+							}
 						}
 
-						var fillFrames = function() {
-							urls = [ getParameterByName("first"),
-									getParameterByName("second"),
-									getParameterByName("third"),
-									getParameterByName("fourth") ];
-							if (urls[0] == "" && urls[1] == "" && urls[2] == ""
-									&& urls[3] == "") {
-								$("#firstFrame").attr('src', "info.html");
-								$("#secondFrame").attr('src', "gen.html");
-								$("#thirdFrame").attr('src', "example.html");
-								$("#fourthFrame").attr('src', "howto.html");
-							} else {
-								if (isURI(urls[0])) {
-									$("#firstFrame").attr('src', urls[0]);
-								} else {
-									$("#firstFrame").attr('srcdoc', urls[0]);
-								}
-								if (isURI(urls[1])) {
-									$("#secondFrame").attr('src', urls[1]);
-								} else {
-									$("#secondFrame").attr('srcdoc', urls[1]);
-								}
-								if (isURI(urls[2])) {
-									$("#thirdFrame").attr('src', urls[2]);
-								} else {
-									$("#thirdFrame").attr('srcdoc', urls[2]);
-								}
-								if (isURI(urls[3])) {
-									$("#fourthFrame").attr('src', urls[3]);
-								} else {
-									$("#fourthFrame").attr('srcdoc', urls[3]);
-								}
-							}
+					};
+					fillFrames();
 
-						};
-						fillFrames();
-
-					});
+				});
